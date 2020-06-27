010d62330e2aec3820c82df392a19283b05e10ae
Janne Kortemäki <janne.kortemaki@helsinki.fi>
2020-06-28 01:20:25 +0300

    eka osa kesken

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -6,6 +6,10 @@ const Header = (props) =>(
   <h2>{props.text}</h2>
   </div>
 )
+
+
+
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -15,8 +19,14 @@ const App = () => {
   return (
     <div>
       <Header text= "Give feedback"/>
+      <button onClick={() => setGood (good +1)}>
+        good 
+      </button>
+      <button onClick={() => setNeutral (neutral +1) </button>
+      <button huono={handleBad}>bad</button>
     </div>
   )
+  
 }
 
 ReactDOM.render(<App />, 
