9fbb5015d66bfce4f9e49c50342bd903bcac267e
Janne Kortemäki <janne.kortemaki@helsinki.fi>
2020-08-19 22:47:21 +0300

    Unicafe, statistiikka puuttuu

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -7,25 +7,75 @@ const Header = (props) =>(
   </div>
 )
 
+const Statistics = ({name, type}) => (
+  <div>
+    <p> {name} {type}</p>
+  </div>
+)
+
+const Button = (props) => {
+  return(
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
 
 
 
 const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+ 
+  const [clicks, setClicks] = useState ({
+    good: 0, neutral: 0, bad: 0, all: 0
+  })
 
-  return (
-    <div>
+  const handleGoodClick = () => 
+    setClicks({ ...clicks, good:clicks.good +1, all:clicks.all +1})
+
+  const handleNeutralClick = () => 
+    setClicks({ ...clicks, neutral:clicks.neutral +1, all:clicks.all +1})
+
+  const handleBadClick = () => 
+    setClicks({ ...clicks, bad:clicks.bad +1, all:clicks.all +1})
+
+  let average = (clicks.good - clicks.bad) / clicks.all
+  let positive = clicks.good / clicks.all * 100
+
+
+  if (clicks.all == 0){
+    return (
+      <>
       <Header text= "Give feedback"/>
-      <button onClick={() => setGood (good +1)}>
-        good 
-      </button>
-      <button onClick={() => setNeutral (neutral +1) </button>
-      <button huono={handleBad}>bad</button>
-    </div>
+    
+      <Button handleClick={handleGoodClick} text="good"/>
+      <Button handleClick={handleNeutralClick} text="neutral"/>
+      <Button handleClick={handleBadClick} text= "bad"/>
+        
+      <Header text="Statistics"/>
+      <p> No feedback given</p>
+      </>
+    )
+  } else {
+  return (
+    
+    <>
+    <Header text= "Give feedback"/>
+    
+    <Button handleClick={handleGoodClick} text="good"/>
+    <Button handleClick={handleNeutralClick} text="neutral"/>
+    <Button handleClick={handleBadClick} text= "bad"/>
+      
+    <Header text="Statistics"/>
+    <Statistics name="good" type={clicks.good}/>
+    <Statistics name="neutral" type={clicks.neutral}/>
+    <Statistics name="bad" type={clicks.bad}/>  
+    <Statistics name="all" type={clicks.all}/>
+    <Statistics name="average" type={average}/>
+    <Statistics name="positive" type={positive}/>
+    
+    </>
   )
+  }  
   
 }
 
